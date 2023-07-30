1a0212e06d60a5da7d08f7141c0c124114ef3a09
sratna <adhikariratna52@gmail.com>
2023-07-30 12:18:08 +0200

    add necessary data models

---
+++ frontend/src/models/ExchangeRate.ts
@@ -0,0 +1,15 @@
+export interface CurrencyCode {
+  code: string;
+  description: string;
+}
+
+export interface ExchangeRateResponse {
+  code: string;
+  rate: string;
+}
+
+export interface ExchangeRate {
+  code: string;
+  description: string;
+  rate: string;
+}
