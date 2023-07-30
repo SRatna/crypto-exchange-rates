70e21b5b1f1bb5a8b769cbaacc65377370b7cd25
sratna <adhikariratna52@gmail.com>
2023-07-30 13:06:50 +0200

    add more crypto with rates

--- backend/src/main/java/com/github/exchangerates/config/LatestRateDataConfig.java
+++ backend/src/main/java/com/github/exchangerates/config/LatestRateDataConfig.java
@@ -10,7 +10,7 @@ import com.github.exchangerates.model.LatestRateData;
 
 @Configuration
 public class LatestRateDataConfig {
-  
+
   @Bean
   public LatestRateData latestRateData() {
     LatestRateData latestRateData = new LatestRateData();
@@ -18,7 +18,11 @@ public class LatestRateDataConfig {
     List<ExchangeRate> exchangeRates = List.of(
         new ExchangeRate("USD", "United States Dollar", 1),
         new ExchangeRate("EUR", "Euro", 1.1),
-        new ExchangeRate("BTC", "Bitcoin", 29307.37));
+        new ExchangeRate("BTC", "Bitcoin", 29307.37),
+        new ExchangeRate("ETH", "Ethereum", 1875),
+        new ExchangeRate("DOGE", "Dogecoin", 0.08),
+        new ExchangeRate("XPR", "Ripple", 0.7217),
+        new ExchangeRate("SOL", "Solana", 24.97));
 
     latestRateData.setExchangeRates(exchangeRates);
     return latestRateData;
