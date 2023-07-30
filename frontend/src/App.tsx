d2384e2e2665c06ff8fd2a53157ff75836403e8b
sratna <adhikariratna52@gmail.com>
2023-07-30 10:10:01 +0200

    add basic layout for the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,40 @@
-function App() {
+import { Layout, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
+import './App.css';
 
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <>
-      <div>Crypto Currency Exchange Rates</div>
-    </>
-  )
-}
-
-export default App
+    <Layout className="layout">
+      <Header>
+        <div className="logo">
+          Crypto Currency Exchange Rates
+        </div>
+      </Header>
+      <Content
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Crypto Currency Exchange Rates App ©2023 Created by SRatna
+      </Footer>
+    </Layout>
+  );
+};
+export default App;
